7f475eb2b0d4b2f488497ade8303d0069160adaa
Takasila Saichol <takkasila@outlook.com>
2015-10-11 15:16:00 +0700

    Update project
    To Unity 5.2.1f

---
+++ Assets/Scripts/CameraAspectControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAspectControl : MonoBehaviour {
+
+    public float orthographicSize = 5;
+    public float aspectRatio = 9 / 16.0f;
+	void Start () {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+            -orthographicSize * aspectRatio, orthographicSize * aspectRatio
+            , -orthographicSize, orthographicSize
+            , 0.3f, 1000
+        );
+	}
+	
+}
