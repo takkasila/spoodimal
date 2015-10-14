539a20aa13b256a13f6151e015cc4dbc8fffb722
Takasila Saichol <takkasila@outlook.com>
2015-10-14 21:24:37 +0700

    Filling backgrounds

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+	}
+}
---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+    public AudioClip sound;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter2D() {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+}
