a547728c8fa9829058a23663d28b1d31f34087cf
Takasila Saichol <takkasila@outlook.com>
2015-10-14 23:33:21 +0700

    Finished scrolling choose pet

---
+++ Assets/Scripts/Menu/ScrollSize.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollSize : MonoBehaviour {
+
+    public float maxSize = 1.2f, minSize = 0.8f;
+    public SpriteRenderer BG_renderer;
+    float screenWidth, screenHeight;
+	void Start () {
+        screenWidth = BG_renderer.bounds.size.x;
+        screenHeight = BG_renderer.bounds.size.y;
+	}
+	
+	void Update () {
+        float dist = Mathf.Clamp(transform.position.x, -screenWidth / 2, screenWidth / 2);
+        float distPercent;
+        if(dist >= 0)
+            distPercent = (screenWidth / 2 - dist) / (screenWidth / 2);
+        else
+            distPercent = (screenWidth / 2 + dist) / (screenWidth / 2);
+
+        float extraSize = (maxSize - minSize) * distPercent;
+        float fullSize = extraSize + minSize;
+        transform.localScale = new Vector3(fullSize, fullSize, fullSize);
+	}
+}
---
+++ Assets/Scripts/Menu/Scrolling.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrolling : MonoBehaviour {
+
+    public float scrollLunchSpeed = 0.8f, frictionAcc = 1f;
+
+    float currentSpeed;
+    float objectLastXPosition;
+    Vector3 mouseLastXPosition;
+
+    bool holding = false;
+    bool lastHoldingFrame = false;
+
+    void Start()
+    {
+        objectLastXPosition = transform.position.x;
+        mouseLastXPosition = Input.mousePosition;
+    }
+    void Update()
+    {
+        scrolling();
+        updateSpeedNPosition();
+    }
+
+    void scrolling()
+    {
+        if (holding)
+        {
+            float deltaMouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x 
+                - Camera.main.ScreenToWorldPoint(mouseLastXPosition).x;
+            foreach(Transform child in transform)
+            {
+                child.position = new Vector3(child.position.x + deltaMouseX, child.position.y, child.position.z);
+            }
+        }
+        else if(lastHoldingFrame)
+        {
+            if ((Input.mousePosition.x - mouseLastXPosition.x) > 0)
+            {
+                currentSpeed = scrollLunchSpeed;
+            }
+            else if ((Input.mousePosition.x - mouseLastXPosition.x) < 0)
+            {
+                currentSpeed = -scrollLunchSpeed;
+            }
+            else
+            {
+                currentSpeed = 0;
+            }
+        }
+
+        lastHoldingFrame = holding;
+        mouseLastXPosition = Input.mousePosition;
+    }
+
+    void updateSpeedNPosition()
+    {
+        if(!holding)
+        {
+            foreach(Transform child in transform)
+            {
+                child.position = new Vector3(child.position.x + currentSpeed * Time.deltaTime, child.position.y, child.position.z);
+            }
+
+            if (currentSpeed > 0)
+            {
+                currentSpeed -= frictionAcc * Time.deltaTime;
+
+                if (currentSpeed < 0)
+                    currentSpeed = 0;
+            }
+            else
+            {
+                currentSpeed += frictionAcc * Time.deltaTime;
+                
+                if(currentSpeed > 0)
+                    currentSpeed = 0;
+            }
+        }
+    }
+
+	void OnMouseDown()
+    {
+        holding = true;
+    }
+
+    void OnMouseUp()
+    {
+        holding = false;
+    }
+}
