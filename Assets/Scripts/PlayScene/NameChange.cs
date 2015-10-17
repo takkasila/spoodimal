2246496b1766c6a916b29a4d1efb9cf7eb6de241
Takasila Saichol <takkasila@outlook.com>
2015-10-17 21:14:37 +0700

    DIscarding change in play.cs

--- Assets/Scripts/PlayScene/NameChange.cs
+++ Assets/Scripts/PlayScene/NameChange.cs
@@ -5,13 +5,11 @@ using System.Collections;
 public class NameChange : MonoBehaviour {
     public Image textFieldImg;
     InputField inputField;
-    Text x;
-	// Use this for initialization
+
 	void Start () {
         inputField = GetComponent<InputField>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
         if (inputField.isFocused)
             textFieldImg.enabled = true;
