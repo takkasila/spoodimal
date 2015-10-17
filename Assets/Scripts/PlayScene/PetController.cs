6f21dde444b6320e3196ac17d6e6c213faf83823
Takasila Saichol <takkasila@outlook.com>
2015-10-17 17:14:26 +0700

    Finished petcontrolling

---
+++ Assets/Scripts/PlayScene/PetController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PetController : MonoBehaviour {
+    public float pushForce = 9.8f;
+    Rigidbody2D rb;
+    Transform forcePoint;
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        Transform centerOfMass =  transform.FindChild("CM");
+        rb.centerOfMass = centerOfMass.localPosition;
+        forcePoint = transform.FindChild("ForcePoint");
+	}
+	
+	void Update () {
+        rb.AddForceAtPosition(new Vector2(Input.acceleration.x * pushForce, 0), forcePoint.position, ForceMode2D.Force);
+	}
+}
