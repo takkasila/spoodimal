21298153334500fb9f4b4a51222e11e08b78309b
Takasila Saichol <takkasila@outlook.com>
2015-10-17 19:21:14 +0700

    Half-finish design playing UI

---
+++ Assets/Scripts/CopyText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CopyText : MonoBehaviour {
+    public Text targetText;
+    Text selfText;
+
+    void Start()
+    {
+        selfText = GetComponent<Text>();
+    }
+    void Update () {
+        selfText.text = targetText.text;
+	}
+}
