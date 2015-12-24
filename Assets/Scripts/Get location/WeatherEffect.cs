7c4c4b7a65883f843a94a94560c8be4573c42c26
Takkasila Saichol <takkasila@outlook.com>
2015-12-24 15:42:14 +0700

    Added sound effect! Finish cycle 2 :package:

--- Assets/Scripts/Get location/WeatherEffect.cs
+++ Assets/Scripts/Get location/WeatherEffect.cs
@@ -9,7 +9,15 @@ public class WeatherEffect : MonoBehaviour {
     public SpriteRenderer BGSprite;
 
     public Sprite sunnyBG;
+    public GameObject sunnyEffect;
+    public AudioClip sunnySoundBG;
+    
     public Sprite rainyBG;
+    public GameObject rainingEffect;
+    public AudioClip rainingSoundBG;
+ 
+    public AudioSource ExtraSoundBG;
+
     void Update()
     {
         if(getlocationguy.weather != "" && !init)
@@ -27,10 +35,16 @@ public class WeatherEffect : MonoBehaviour {
         {
             case "Rain":
                 BGSprite.sprite = rainyBG;
+                Instantiate(rainingEffect);
+                ExtraSoundBG.clip = rainingSoundBG;
+                ExtraSoundBG.Play();
                 break;
 
             default:
                 BGSprite.sprite = sunnyBG;
+                Instantiate(sunnyEffect);
+                ExtraSoundBG.clip = sunnySoundBG;
+                ExtraSoundBG.Play();
                 break;
         }
     }
---
+++ Assets/Scripts/PlayScene/SunnyEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunnyEffect : MonoBehaviour {
+    public float step;
+    float counter;
+    public float tickTime;
+	// Update is called once per frame
+	void Update () {
+        if (counter < tickTime)
+            counter += Time.deltaTime;
+        else
+        {
+            counter = 0;
+            transform.Rotate(new Vector3(0, 0, 1) * step);
+        }
+	}
+}
